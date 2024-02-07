8153dda462467ef3f54c0c6a2e80c748ba1227b0
xushengs <xushengs@gmail.com>
2024-02-07 12:47:27 +0800

    fix: fixes #5 Start and End Delimiter

--- lib/src/latex_inline_syntax.dart
+++ lib/src/latex_inline_syntax.dart
@@ -1,15 +1,16 @@
 import 'package:markdown/markdown.dart';
 
 class LatexInlineSyntax extends InlineSyntax {
+  // (?:(\${1,2})(?!\$)((?:\\.|[^\\\n])*?(?:\\.|[^\\\n\$]))\1(?=[\s?!\.,:？！。，：]|$))
+  // (?:(\\\()(?!\\\))((?:\\.|[^\\\n])*?(?:\\.|[^\\\n\\\)]))\\\)(?=[\s?!\.,:？！。，：]|$))
   LatexInlineSyntax()
       : super(
-            r'(\${1,2})(?!\$)((?:\\.|[^\\\n])*?(?:\\.|[^\\\n\$]))\1(?=[\s?!\.,:？！。，：]|$)');
+            r'(?:(\${1,2})(?!\$)((?:\\.|[^\\\n])*?(?:\\.|[^\\\n\$]))\1(?=[\s?!\.,:？！。，：]|$))|(?:(\\\()(?!\\\))((?:\\.|[^\\\n])*?(?:\\.|[^\\\n\\\)]))\\\)(?=[\s?!\.,:？！。，：]|$))');
 
   @override
   bool onMatch(InlineParser parser, Match match) {
-    Element element = Element.text('latex', match[2] ?? '');
-    element.attributes['displayMode'] =
-        match[1]?.length == 2 ? 'true' : 'false';
+    Element element = Element.text('latex', match[2] ?? (match[4] ?? ''));
+    element.attributes['displayMode'] = match[1] == '\$\$' ? 'true' : 'false';
     parser.addNode(element);
     return true;
   }
