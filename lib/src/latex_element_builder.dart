d884604cd6ac9d8e79ad2763dce4b5f997838594
patrick <patrick.zierahn@student.kit.edu>
2024-02-14 20:19:40 +0100

    Clean up LatexElementBuilder

--- lib/src/latex_element_builder.dart
+++ lib/src/latex_element_builder.dart
@@ -4,18 +4,23 @@ import 'package:flutter_math_fork/flutter_math.dart';
 import 'package:markdown/markdown.dart' as md;
 
 class LatexElementBuilder extends MarkdownElementBuilder {
-  TextStyle? textStyle;
-
   LatexElementBuilder({this.textStyle});
 
+  final TextStyle? textStyle;
+
   @override
-  Widget visitElementAfterWithContext(BuildContext context, md.Element element,
-      TextStyle? preferredStyle, TextStyle? parentStyle) {
-    final String? displayMode = element.attributes['displayMode'];
+  Widget visitElementAfterWithContext(
+    BuildContext context,
+    md.Element element,
+    TextStyle? preferredStyle,
+    TextStyle? parentStyle,
+  ) {
     final String text = element.textContent;
     if (text.isEmpty) {
       return const SizedBox();
     }
+
+    final displayMode = element.attributes['displayMode'];
 
     return Math.tex(
       text,
