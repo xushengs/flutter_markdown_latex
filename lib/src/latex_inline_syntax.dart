4436d09c9eb16aa71a478ad63f0b9fd95adfef4d
patrick <patrick.zierahn@student.kit.edu>
2024-02-14 20:30:38 +0100

    Simplified regex and clean up code

--- lib/src/latex_inline_syntax.dart
+++ lib/src/latex_inline_syntax.dart
@@ -1,17 +1,26 @@
 import 'package:markdown/markdown.dart';
 
+const _latexPattern =
+    r'(\\\$|(?<!\\)\${1,2}|\\(?:\(|\[))(.*?)(\\(?:\)|\])|(?<!\\)\${1,2})';
+
 class LatexInlineSyntax extends InlineSyntax {
-  // (?:(\${1,2})(?!\$)((?:\\.|[^\\\n])*?(?:\\.|[^\\\n\$]))\1(?=[\s?!\.,:？！。，：]|$))
-  // (?:(\\\()(?!\\\))((?:\\.|[^\\\n])*?(?:\\.|[^\\\n\\\)]))\\\)(?=[\s?!\.,:？！。，：]|$))
-  LatexInlineSyntax()
-      : super(
-            r'(?:(\${1,2})(?!\$)((?:\\.|[^\\\n])*?(?:\\.|[^\\\n\$]))\1(?=[\s?!\.,:？！。，：]|$))|(?:(\\\()(?!\\\))((?:\\.|[^\\\n])*?(?:\\.|[^\\\n\\\)]))\\\)(?=[\s?!\.,:？！。，：]|$))');
+  LatexInlineSyntax() : super(_latexPattern);
 
   @override
   bool onMatch(InlineParser parser, Match match) {
-    Element element = Element.text('latex', match[2] ?? (match[4] ?? ''));
-    element.attributes['displayMode'] = match[1] == '\$\$' ? 'true' : 'false';
+    if (match.groupCount < 3) {
+      return false;
+    }
+
+    final equation = (match[2] ?? '').trim();
+    if (equation.isEmpty) {
+      return false;
+    }
+
+    final element = Element.text('latex', equation);
+    element.attributes['displayMode'] = equation;
     parser.addNode(element);
+
     return true;
   }
 }
